0ef2c3531a02ac519529cf6bcd72934c0c7795cf
Fayyadh Arrazan Miftakhul <113814875+arrazan7@users.noreply.github.com>
2023-10-11 09:16:00 +0700

    second commit

--- routes/web.php
+++ routes/web.php
@@ -16,13 +16,13 @@ use App\Http\Controllers\Product;
 
 Route::get('/', function () {return view('welcome');});
 
-Route::get('/website_pemain', [Product::class,'index']);
+Route::get('/website_minimarket', [Product::class,'index']);
 
 //Create
 Route::get('/website_minimarket/create', [Product::class,'create'])->name('membuat_data');
 
 //Store Create
-Route::post('/website_pemain', [Product::class,'store'])->name('menyimpan_data');
+Route::post('/website_minimarket', [Product::class,'store'])->name('menyimpan_data');
 
 //Update
 Route::get('/website_minimarket/edit/{idsss}', [Product::class, 'edit'])->name('mengedit_data');
